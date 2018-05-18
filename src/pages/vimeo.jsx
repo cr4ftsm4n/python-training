695f9d419cad1654413ae5d9a1c000159392bcbd
cr4ftsm4n <mou.hangun@gmail.com>
2018-05-18 13:09:20 +0900

    change layout

--- src/pages/vimeo.jsx
+++ src/pages/vimeo.jsx
@@ -4,39 +4,31 @@ import Link from 'gatsby-link'
 const Vimeo = () => (
   <div>
     <section className="section">
-      <div className="container">
-        <h1 className="title">4/26</h1>
-        <iframe
-          src="https://player.vimeo.com/video/266986347"
-          width="640"
-          height="400"
-          frameborder="0"
-          allowFullScreen
-        />
-      </div>
-    </section>
-    <section className="section">
-      <div className="container">
-        <h1 className="title">travis</h1>
-        <iframe
-          src="https://player.vimeo.com/video/268998577"
-          width="640"
-          height="400"
-          frameborder="0"
-          allowFullScreen
-        />
-      </div>
-    </section>
-    <section className="section">
-      <div className="container">
-        <h1 className="title">Pull Request</h1>
-        <iframe
-          src="https://player.vimeo.com/video/270117041"
-          width="640"
-          height="400"
-          frameborder="0"
-          allowFullScreen
-        />
+      <div className="columns">
+        <div className="column">
+          <h1 className="title">4/26</h1>
+          <iframe
+            src="https://player.vimeo.com/video/266986347"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+        <div className="column">
+          <h1 className="title">travis</h1>
+          <iframe
+            src="https://player.vimeo.com/video/268998577"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+        <div className="column">
+          <h1 className="title">Pull Request</h1>
+          <iframe
+            src="https://player.vimeo.com/video/270117041"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
       </div>
     </section>
   </div>
